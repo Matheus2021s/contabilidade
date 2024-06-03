8da23fcea0d9847a8c1c066efb7ea580debe664e
Matheus2021s <matheusmartinsdacosta21@gmail.com>
2024-06-02 23:41:48 -0300

    intitial commit

---
+++ src/test/java/br/com/mariah/contabilidade/ContabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariah.contabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
